35b0f5b133063531f112b7143007a6890959ebca
amee <muhammadalmer2304@gmail.com>
2026-02-03 19:47:03 +0700

    membuat component untuk feeds

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,20 @@ function Body() {
 
 function Feeds() {
   return (
-    <div style={{ backgroundColor: 'grey', width: 100, display: 'flex', justifyContent: 'center' }}>
-      <span>hallo</span>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <Feed />
     </div>
   );
 }
+
+function Feed() {
+  return(
+    <div style={{backgroundColor: 'grey' ,width: 100, display:'flex', justifyContent: 'center', height: 550, width: 450, alignItems: 'center', marginTop: 50}}>
+      <span>Hallo</span>
+    </div>
+  );
+
+}  
 
 function Sidebar() {
   return <></>;
