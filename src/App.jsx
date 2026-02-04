6f5809f4a5f8eb93659a127cd63946067ddc250f
amee <muhammadalmer2304@gmail.com>
2026-02-04 14:34:25 +0700

    nambahin sidebar dan iconnya

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
+import igIcon from './assets/instagram.png';
+import homeIcon from './assets/home.png';
+import playIcon from './assets/play.png';
+import messageIcon from './assets/send.png';
+import searchIcon from './assets/search-interface-symbol.png';
+import compassIcon from './assets/compass-circular-tool.png';
+import likeIcon from './assets/heart.png';
+import createIcon from './assets/plus.png';
+import profileIcon from './assets/user.png';
+import burgerIcon from './assets/menu.png';
+
 function App() {
-  return <Body />;
+  return (
+      <Body />
+      
+  )
 }
 
 function Body() {
   return (
-    <div style={{display: 'flex', flexDirection: 'row', justifyContent:'center', width: 'full'}}>
-      <div style={{display:'flex', justifyContent: 'start'}}>
-        <Sidebar/>
+    <div style={{display: 'flex', flexDirection: 'row', width: '100%'}}>
+      <div style={{display: 'flex', alignItems: 'flex-start', flexDirection: 'column'}}>
+        <Sidebar icon={igIcon}/>
+        <Sidebar icon={homeIcon} label="Home"/>
+        <Sidebar icon={playIcon} label="Reels"/>
+        <Sidebar icon={messageIcon} label="Messages"/>
+        <Sidebar icon={searchIcon} label="Search"/>
+        <Sidebar icon={compassIcon} label="Explore"/>
+        <Sidebar icon={likeIcon} label="Notification"/>
+        <Sidebar icon={createIcon} label="Create"/>
+        <Sidebar icon={profileIcon} label="Profile"/>
+        <Sidebar icon={burgerIcon} label="More"/>
       </div>
-      <div >
+      <div style={{flex:1}}>
         <Stories />
         <Feeds />
       </div>
-      <div>
+      <div style={{width:300, marginRight: 100}}>
         <RightSidebar/>
         <RightSidebar/>
         <RightSidebar/>
@@ -44,7 +67,7 @@ function Feed() {
 
 function RightSidebar(){
   return(
-    <div style={{display: 'flex', flexDirection:'row', marginLeft:75}}>
+    <div style={{display: 'flex', flexDirection:'row'}}>
       <div style={{backgroundColor:'grey', width:75, height: 75,borderRadius:100, display:'flex', alignItems:'center', justifyContent:'center', marginTop: 30}}>
         <h6>contoh profile</h6>
       </div>
@@ -59,11 +82,14 @@ function RightSidebar(){
   );
 }
 
-function Sidebar() {
+function Sidebar(props) {
   return( 
-    <div style={{display:'flex', marginLeft: 10}}>
-      <div style={{display:'flex', justifyContent:'start'}}>
-        <span>beranda</span>
+    <div style={{display:'flex'}}>
+      <div style={{display:'flex', justifyContent:'start', marginTop:50}}>
+        <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+          <img src = {props.icon} style={{width: 30, height: 30}}/>
+          <label style={{fontSize: 25, marginLeft: 10}}>{props.label}</label>
+        </div>
       </div>
     </div>
   );
@@ -93,6 +119,7 @@ function Story() {
         padding: 50,
         margin: 10,
         display: 'inline',
+        position: 'relative',
       }}
     >
       {/* <span>Contoh Story</span> */}
